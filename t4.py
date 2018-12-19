30937de4f7c8551ba62f0a556169c3abb4cba81c
erjcan <erjcan@gmail.com>
2018-12-19 21:06:04 +0500

    t4 was modified - added some comments

--- t4.py
+++ t4.py
@@ -1,45 +1,18 @@
-import smtplib
+#decorator
+def my_decorator(func):
+    def f(x,y=5):
+        rv = func(x,y)
+        print("inside decorator!")
+        return rv
+    return f
 
-host ="smtp.gmail.com"
-port =587
-username="katiematri7@gmail.com"
-password = "Tavoluku25"
+@my_decorator
+def sub(x,y =10):
+    return x-y
+@my_decorator
+def add(x,y=7):
+    return x+y
 
-from_email = username
-to_list = [ "hivauz@gmail.com"]
-
-email_conn = smtplib.SMTP(host,port)
-email_conn.ehlo()
-email_conn.starttls()
-email_conn.login(username,password)
-
-
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-
-the_msg = MIMEMultipart("alternative")
-the_msg['Subject'] = "helo there"
-the_msg["From"] = from_email
-the_msg["To"] = to_list[0]
-plain_txt = "testing the msg"
-html_txt = """\
-<html>
-    <head> </head>
-    <body>
-        <p>Hey! <br>
-        testgin this email <b> mesg </b>. Made by <a href='https;dfs'> team erj</a>
-        </p>
-    </body>
-</html>
-    """
-
-part1 = MIMEText(plain_txt,'plain')
-part2 = MIMEText(html_txt, "html")
-
-the_msg.attach(part1)
-the_msg.attach(part2)
-
-print(the_msg.as_string())
-
-email_conn.sendmail(from_email, to_list, the_msg.as_string())
-email_conn.quit()
+#print('add function using decorator!' , add(10,56))
+for _ in range(4):
+    print(4)
